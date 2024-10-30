bc902aef890826dd6ff70e3652f720188c424f48
hector <alicehector5211314@gmail.com>
2024-10-30 11:27:05 +0800

    init

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5199072c'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.jdkmqmeem4o"
+    "revision": "0.g1aeugbdhj"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
